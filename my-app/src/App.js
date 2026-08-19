6ad31ee84a8712fbc32d1cf7424087d14ebb51e9
Ryu JungKyun <164990532+ryujungkyun@users.noreply.github.com>
2026-08-20 00:22:57 +0900

    Update link text from 'Learn GithubAction3' to 'Learn GithubAction cicd1 test'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction3
+          Learn GithubAction cicd1 test
         </a>
       </header>
     </div>
